0d295740d676c792ed3b37d332903560396c89e1
ThaoNt <thaont@rikkeisfot.com>
2018-11-29 18:16:38 +0700

    add task

--- root/js/main.js
+++ root/js/main.js
@@ -1,40 +1,65 @@
 let todo = [{
-  name: 'task1',
-  place: 'place1',
+  name: 'Do tasks at office',
+  place: 'office',
   type: 'business',
   isDone: true
 }, {
-  name: 'task2',
-  place: 'place2',
+  name: 'go to market',
+  place: 'market',
   type: 'personal',
   isDone: false
 }, {
-  name: 'task3',
-  place: 'place3',
+  name: 'meet customers',
+  place: 'cafe house',
   type: 'business',
+  isDone: false
+}, {
+  name: 'walk a dog',
+  place: 'park',
+  type: 'personal',
   isDone: false
 }];
 let ul = document.getElementById('list');
 
 function load() {
-  if (!localStorage.getItem('todos')) {
-    localStorage.setItem('todos', JSON.stringify(todo));
+  let data = localStorage.getItem('todo');
+
+  if (!data) {
+    localStorage.setItem('todo', JSON.stringify(todo));
   }
 
+  todo = JSON.parse(data);
   let innerList = '';
 
   for (let i = 0; i < todo.length; i++) {
     innerList += `<li class="item">
-                <div class="item__illus">
-                  <div class="item__type"> ${todo[i].type} </div>
-                </div>
-                <div class="item__content">
-                  <h3 class="item__name"> ${todo[i].name} </h3>
-                  <p class="item__place"> ${todo[i].place} </p>
-                </div>
-              </li>`;
+                    <div class="item__illus">
+                      <div class="item__type"> ${todo[i].type} </div>
+                    </div>
+                    <div class="item__content">
+                      <h3 class="item__name"> ${todo[i].name} </h3>
+                      <p class="item__place"> ${todo[i].place} </p>
+                    </div>
+                    <div class="method">
+                      <span class="method__edit" onclick="editTask(${i})">
+                        <img src="images/edit.png" alt="">
+                      </span>
+                      <span class="method__del" onclick="delTask(${i})">
+                        <img src="images/del.png" alt="">
+                      </span>
+                    </div>
+                  </li>`;
     ul.innerHTML = innerList;
   }
+
+  let business = todo.reduce(function (acc, item) {
+    return acc + (item.type === 'business');
+  }, 0);
+  console.log(business);
+  let personal = todo.reduce(function (acc, item) {
+    return acc + (item.type === 'personal');
+  }, 0);
+  console.log(personal);
 }
 
 let plus = document.getElementById('plus');
@@ -46,13 +71,73 @@ plus.addEventListener('click', function () {
 back.addEventListener('click', function () {
   modal.classList.remove('show-modal');
 });
-let input1 = document.getElementById('input1');
-let input2 = document.getElementById('input2');
+let name = document.getElementById('task-name');
+let place = document.getElementById('task-place');
+let type = document.getElementById('select-type');
+let error_name = document.getElementById('error-name');
+let error_place = document.getElementById('error-place');
+let errorName = false;
+let errorPlace = false;
 
-function addTask() {
-  if (input1.value && input2.value) {
-    todo.push();
+function validateName() {
+  if (name.value.length <= 0) {
+    error_name.innerHTML = "Required";
+    errorName = true;
+  } else {
+    errorName = false;
+    error_name.innerHTML = "";
   }
 }
+
+function validatePlace() {
+  if (place.value.length <= 0) {
+    error_place.innerHTML = "Required";
+    errorPlace = true;
+  } else {
+    errorPlace = false;
+    error_place.innerHTML = "";
+  }
+}
+
+function addTask() {
+  validateName();
+  validatePlace();
+
+  if (errorName === false && errorPlace === false) {
+    let newTask = {
+      name: name.value,
+      place: place.value,
+      type: type.value,
+      isDone: false
+    };
+    todo.unshift(newTask);
+    localStorage.setItem('todo', JSON.stringify(todo));
+    modal.classList.remove('show-modal');
+    name.value = '';
+    place.value = '';
+  }
+
+  load();
+}
+
+function delName() {
+  name.value = '';
+}
+
+function delPlace() {
+  place.value = '';
+}
+
+function delTask(i) {
+  todo.splice(i, 1);
+  localStorage.setItem('todo', JSON.stringify(todo));
+  load();
+} // function editTask(i) {
+//   let editTask = todo[i];
+//   name.value = editTask.name;
+//   place.value = editTask.place;
+//   type.value = editTask.type;
+// }
+
 
 load();
